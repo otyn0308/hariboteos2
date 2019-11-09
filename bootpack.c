ca7df7db6040ba4e0538e2a421002f5662e6f44d
otyn0308 <elements0308@gmail.com>
2019-11-09 10:59:55 +0900

    4-6

--- bootpack.c
+++ bootpack.c
@@ -1,8 +1,16 @@
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void){
   int i;
   char *p;
+  init_palette();
   p = (char *) 0xa0000;
   for(i = 0; i <= 0xaffff; i++){
    p[i] = i & 0x0f;
@@ -12,3 +20,40 @@ void HariMain(void){
   }
 }
 
+void init_palette(void){
+  static unsigned char table_rgb[16 * 3] = {
+    0x00, 0x00, 0x00,   // 0:black
+    0xff, 0x00, 0x00,   // 1:bright led
+    0x00, 0xff, 0x00,   // 2:bright green
+    0xff, 0xff, 0x00,   // 3:bright yellow
+    0x00, 0x00, 0xff,   // 4:bright blue
+    0xff, 0x00, 0xff,   // 5:bright purple
+    0x00, 0xff, 0xff,   // 6:bright light blue
+    0xff, 0xff, 0xff,   // 7:white
+    0xc6, 0xc6, 0xc6,   // 8:bright gray
+    0x84, 0x00, 0x00,   // 9:dark led
+    0x00, 0x84, 0x00,   //10:dark green
+    0x84, 0x84, 0x00,   //11:dark yellow
+    0x00, 0x00, 0x84,   //12:dark blue
+    0x84, 0x00, 0x84,   //13:dark purple
+    0x00, 0x84, 0x84,   //14:dark light blue
+    0x84, 0x84, 0x84    //15 darl gray
+  };
+  set_palette(0, 15, table_rgb);
+  return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+  int i, eflags;
+  eflags = io_load_eflags();
+  io_cli();
+  io_out8(0x03c8, start);
+  for(i = start; i <= end; i++){
+    io_out8(0x03c9, rgb[0] / 4);
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+    rgb += 3;
+  }
+  io_store_eflags(eflags);
+  return;
+}
--- naskfunc.nas
+++ naskfunc.nas
@@ -6,7 +6,10 @@
 [BITS 32]                   ;32ビットモード用の機械語を作らせる
 [FILE "naskfunc.nas"]       ;ソースファイル名情報
 
-        GLOBAL _io_hlt
+        GLOBAL  _io_hlt, _io_cli, _io_sti, _io_stihlt
+        GLOBAL  _io_in8, _io_in16, _io_in32
+        GLOBAL  _io_out8, _io_out16, _io_out32
+        GLOBAL  _io_load_eflags, _io_store_eflags
 
 [SECTION .text]
 
@@ -14,3 +17,61 @@ _io_hlt:
         HLT
         RET
 
+_io_cli:
+        CLI
+        RET
+
+_io_sti:
+        STI
+        RET
+
+_io_stihlt
+        STI
+        HLT
+        RET
+
+_io_in8:
+        MOV     EDX,[ESP+4]
+        MOV     EAX,0
+        IN      AL,DX
+        RET
+
+_io_in16:
+        MOV     EDX,[ESP+4]
+        MOV     EAX,0
+        IN      AX,DX
+        RET
+
+_io_in32:
+        MOV     EDX,[ESP+4]
+        IN      EAX,DX
+        RET
+
+_io_out8:
+        MOV     EDX,[ESP+4]
+        MOV     AL,[ESP+8]
+        OUT     DX,AL
+        RET
+
+_io_out16:
+        MOV     EDX,[ESP+4]
+        MOV     EAX,[ESP+8]
+        OUT     DX,AX
+        RET
+
+_io_out32:
+        MOV     EDX,[ESP+4]
+        MOV     EAX,[ESP+8]
+        OUT     DX,EAX
+        RET
+
+_io_load_eflags:
+        PUSHFD              ;PUSH EFLAGS
+        POP     EAX
+        RET
+
+_io_store_eflags:
+        MOV     EAX,[ESP+4]
+        PUSH    EAX
+        POPFD               ;POP EFLAGS
+        RET
