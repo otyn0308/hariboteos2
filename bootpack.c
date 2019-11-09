16958e1d67fab712a7f5f33c9f7456873d20558e
otyn0308 <elements0308@gmail.com>
2019-11-09 09:52:32 +0900

    4-4

--- bootpack.c
+++ bootpack.c
@@ -3,10 +3,9 @@ void io_hlt(void);
 void HariMain(void){
   int i;
   char *p;
-
-  for(i = 0xa0000; i <= 0xaffff; i++){
-    p = i;
-    *p = i & 0x0f;
+  p = (char *) 0xa0000;
+  for(i = 0; i <= 0xaffff; i++){
+   *(p + i) = i & 0x0f;
   }  
   for(;;){
     io_hlt();
