88dca3b2df373d15378b0fec482411e30e9093a9
otyn0308 <elements0308@gmail.com>
2019-11-13 01:38:12 +0900

    7-2

--- bootpack.c
+++ bootpack.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+extern struct KEYBUF keybuf;
+
 void HariMain(void){
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256];
-  int mx, my;
+  int mx, my, i;
 
   init_gdtidt();
   init_pic();
   io_sti();
+
+  io_out8(PIC0_IMR, 0xf9);
+  io_out8(PIC1_IMR, 0xef);
 
   init_palette();
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -19,11 +24,18 @@ void HariMain(void){
   sprintf(s, "(%d, %d)", mx, my);
   putfont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-  io_out8(PIC0_IMR, 0xf9);
-  io_out8(PIC1_IMR, 0xef);
-
   for(;;){
-    io_hlt();
+    io_cli();
+    if(keybuf.flag == 0){
+      io_stihlt();
+    }else{
+      i = keybuf.data;
+      keybuf.flag = 0;
+      io_sti();
+      sprintf(s, "%02X", i);
+      boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+      putfont8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+    }
   }
 }
 
