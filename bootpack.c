05800e0d30dc47c39079a7cb52fe83c1ae4a23e9
otyn0308 <elements0308@gmail.com>
2019-11-13 19:48:49 +0900

    7-4

--- bootpack.c
+++ bootpack.c
@@ -6,7 +6,7 @@ extern struct KEYBUF keybuf;
 void HariMain(void){
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256];
-  int mx, my, i, j;
+  int mx, my, i;
 
   init_gdtidt();
   init_pic();
@@ -26,13 +26,14 @@ void HariMain(void){
 
   for(;;){
     io_cli();
-    if(keybuf.next == 0){
+    if(keybuf.len == 0){
       io_stihlt();
     }else{
-      i = keybuf.data[0];
-      keybuf.next--;
-      for(j = 0; j < keybuf.next; j++){
-        keybuf.data[j] = keybuf.data[j + 1];
+      i = keybuf.data[keybuf.next_r];
+      keybuf.len--;
+      keybuf.next_r++;
+      if(keybuf.next_r == 32){
+        keybuf.next_r = 0;
       }
       io_sti();
       sprintf(s, "%02X", i);
