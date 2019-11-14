2d271917f2223047c5b90689e4356e41a0a84e04
otyn0308 <elements0308@gmail.com>
2019-11-15 01:32:19 +0900

    7-7

--- bootpack.h
+++ bootpack.h
@@ -7,7 +7,7 @@ struct BOOTINFO{
   short scrnx, scrny;
   char *vram;
 };
-#define ADR_BOOTINFO  0x00000ff0
+#define ADR_BOOTINFO    0x00000ff0
 
 //naskfunc.nas
 void io_hlt(void);
@@ -75,10 +75,10 @@ struct GATE_DESCRIPTOR{
 void init_gdtidt(void);
 void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
 void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-#define ADR_IDT      0x0026f800
-#define LIMIT_IDT    0x000007ff
-#define ADR_GDT      0x00270000
-#define LIMIT_GDT    0x0000ffff
+#define ADR_IDT       0x0026f800
+#define LIMIT_IDT     0x000007ff
+#define ADR_GDT       0x00270000
+#define LIMIT_GDT     0x0000ffff
 #define ADR_BOTPAK    0x00280000
 #define LIMIT_BOTPAK  0x0007ffff
 #define AR_DATA32_RW  0x4092
@@ -92,13 +92,13 @@ void inthandler27(int *esp);
 void inthandler2c(int *esp);
 #define PIC0_ICW1    0x0020
 #define PIC0_OCW2    0x0020
-#define PIC0_IMR    0x0021
+#define PIC0_IMR     0x0021
 #define PIC0_ICW2    0x0021
 #define PIC0_ICW3    0x0021
 #define PIC0_ICW4    0x0021
 #define PIC1_ICW1    0x00a0
 #define PIC1_OCW2    0x00a0
-#define PIC1_IMR    0x00a1
+#define PIC1_IMR     0x00a1
 #define PIC1_ICW2    0x00a1
 #define PIC1_ICW3    0x00a1
 #define PIC1_ICW4    0x00a1
