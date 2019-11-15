4a7e0b89ea649b911dce67e0cce24b8fa296fcae
otyn0308 <elements0308@gmail.com>
2019-11-15 11:23:07 +0900

    8-3

--- bootpack.c
+++ bootpack.c
@@ -53,8 +53,17 @@ void HariMain(void){
         i = fifo8_get(&mousefifo);
         io_sti();
         if(mouse_decode(&mdec, i) != 0){
-          sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+          sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+          if((mdec.btn & 0x01) != 0){
+            s[1] = 'L';
+          }
+          if((mdec.btn & 0x02) != 0){
+            s[3] = 'R';
+          }
+          if((mdec.btn & 0x04) != 0){
+            s[2] = 'C';
+          }
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
         }
       }
@@ -62,12 +71,12 @@ void HariMain(void){
   }
 }
 
-#define PORT_KEYDAT        0x0060
-#define PORT_KEYSTA        0x0064
-#define PORT_KEYCMD        0x0064
-#define KEYSTA_SEND_NOTREADY  0x02
-#define KEYCMD_WRITE_MODE    0x60
-#define KBC_MODE        0x47
+#define PORT_KEYDAT             0x0060
+#define PORT_KEYSTA             0x0064
+#define PORT_KEYCMD             0x0064
+#define KEYSTA_SEND_NOTREADY    0x02
+#define KEYCMD_WRITE_MODE       0x60
+#define KBC_MODE                0x47
 
 void wait_KBC_sendready(void){
   for(;;){
@@ -106,8 +115,10 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
     return 0;
   }
   if(mdec->phase == 1){
-    mdec->buf[0] = dat;
-    mdec->phase = 2;
+    if((dat & 0xc8) == 0x08){
+      mdec->buf[0] = dat;
+      mdec->phase = 2;
+    }
     return 0;
   }
   if(mdec->phase == 2){
@@ -118,6 +129,16 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
   if(mdec->phase == 3){
     mdec->buf[2] = dat;
     mdec->phase = 1;
+    mdec->btn = mdec->buf[0] & 0x07;
+    mdec->x = mdec->buf[1];
+    mdec->y = mdec->buf[2];
+    if((mdec->buf[0] & 0x10) != 0){
+      mdec->x |= 0xffffff00;
+    }
+    if((mdec->buf[0] & 0x20) != 0){
+      mdec->y |= 0xffffff00;
+    }
+    mdec->y = - mdec->y;
     return 1;
   }
   return -1;
